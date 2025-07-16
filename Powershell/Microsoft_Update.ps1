3156af3ea711b1389406e760f3e8485c11eff25c
hamsammich00 <landonmurphrey@gmail.com>
2025-07-15 23:15:03 -0500

    Update Microsoft_Update.ps1

--- Powershell/Microsoft_Update.ps1
+++ Powershell/Microsoft_Update.ps1
@@ -1,5 +1,6 @@
 #Description
 #Checks if the PSWindowsUpdate module is installed and installs it if it is not already, then runs the install for all Microsoft Updates
+# article related https://hamsammich00.github.io/posts/Please-Update-Your-Stuff/
 
 #Check if the module is installed, then continue with the updates
 $ModuleName = "PSWindowsUpdate"
